4309fcdb675a6dd2f52d277825d02681787951a6
Orest Tokovenko <hi@oresttokovenko.com>
2024-07-06 22:51:43 -0600

    adding functionality to copy directories within _template

--- resume_workflow/helper.py
+++ resume_workflow/helper.py
@@ -8,20 +8,33 @@ JOB_DESCRIPTION_FILE: str = "job_description.txt"
 TEMPLATE_DIR: Path = Path("_template")
 
 
-def create_dir_and_files(
-    company_name: str, job_title: str, use_template: bool = True
-) -> None:
-    job_title_str = (
-        job_title.lower().replace(" ", "_").replace(",", "_").replace("__", "_")
-    )
+def copy_template(template_dir_path: Path, job_dir: Path, use_template: bool) -> None:
+    """
+    Copies the entire template directory to the job directory
 
-    root = Path(".")
-    template_dir_path = root / TEMPLATE_DIR
-    company_name_dir = Path(company_name)
-    job_dir = company_name / Path(job_title_str)
+    :param template_dir_path: Path to the template directory
+    :param job_dir: Path to the job directory
+    """
+    if template_dir_path.exists() and use_template:
+        shutil.copytree(template_dir_path, job_dir, dirs_exist_ok=True)
+        logger.info(f"copying entire template directory from {template_dir_path} to {job_dir}")
 
-    job_description = Path(JOB_DESCRIPTION_FILE)
-    job_description_dir = job_dir / job_description
+
+def create_dir_and_files(company_name: str, job_title: str, use_template: bool = True) -> None:
+    """
+    Creates the structure for a new resume directory
+
+    :param company_name: The name of the company
+    :param job_title: The title of the job
+    :param use_template: A boolean which indicates if the template dir should be used
+    """
+    job_title_str: str = job_title.lower().replace(" ", "_").replace(",", "_").replace("__", "_")
+
+    company_name_dir: Path = Path(company_name)
+    job_dir: Path = company_name / Path(job_title_str)
+
+    job_description: Path = Path(JOB_DESCRIPTION_FILE)
+    job_description_dir: Path = job_dir / job_description
 
     try:
         # create company dir
@@ -35,12 +48,8 @@ def create_dir_and_files(
         # create job requirements file
         job_description_dir.touch(exist_ok=False)
 
-        # if using template, copy files in _template
-        if use_template and TEMPLATE_DIR.exists():
-            template_files = [x.name for x in template_dir_path.iterdir()]
-            for file in template_files:
-                shutil.copy(TEMPLATE_DIR / file, job_dir / file)
-                logger.info(f"copying {file} from template directory")
+        # if using template
+        copy_template(TEMPLATE_DIR, job_dir, use_template)
 
     except FileExistsError:
         logger.info(
@@ -54,12 +63,8 @@ def create_dir_and_files(
             # create job requirements file
             job_description_dir.touch(exist_ok=False)
 
-            # if using template, copy files in _template
-            if use_template and TEMPLATE_DIR.exists():
-                template_files = [x.name for x in template_dir_path.iterdir()]
-                for file in template_files:
-                    shutil.copy(TEMPLATE_DIR / file, job_dir / file)
-                    logger.info(f"copying {file} from template directory")
+            # if using template
+            copy_template(TEMPLATE_DIR, job_dir, use_template)
 
         except FileExistsError:
             logger.info(
